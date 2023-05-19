04349d8c068fddfea30d5dda5d6d70b941e2c8b9
ProfAtreides <dambrozewicz7@gmail.com>
2023-05-19 13:10:59 +0200

    I wont even pretend I know what the hell is wrong

---
+++ cmake-build-debug/CMakeFiles/struktury_danych.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/dambr/CLionProjects/data_structure-project/source/graph_adjacent_list.cpp" "CMakeFiles/struktury_danych.dir/source/graph_adjacent_list.cpp.obj" "gcc" "CMakeFiles/struktury_danych.dir/source/graph_adjacent_list.cpp.obj.d"
+  "C:/Users/dambr/CLionProjects/data_structure-project/source/graph_two_dim_array.cpp" "CMakeFiles/struktury_danych.dir/source/graph_two_dim_array.cpp.obj" "gcc" "CMakeFiles/struktury_danych.dir/source/graph_two_dim_array.cpp.obj.d"
+  "C:/Users/dambr/CLionProjects/data_structure-project/source/graph_vector_adjacent.cpp" "CMakeFiles/struktury_danych.dir/source/graph_vector_adjacent.cpp.obj" "gcc" "CMakeFiles/struktury_danych.dir/source/graph_vector_adjacent.cpp.obj.d"
+  "C:/Users/dambr/CLionProjects/data_structure-project/source/main.cpp" "CMakeFiles/struktury_danych.dir/source/main.cpp.obj" "gcc" "CMakeFiles/struktury_danych.dir/source/main.cpp.obj.d"
+  "C:/Users/dambr/CLionProjects/data_structure-project/source/pair.cpp" "CMakeFiles/struktury_danych.dir/source/pair.cpp.obj" "gcc" "CMakeFiles/struktury_danych.dir/source/pair.cpp.obj.d"
+  "C:/Users/dambr/CLionProjects/data_structure-project/source/vector.cpp" "CMakeFiles/struktury_danych.dir/source/vector.cpp.obj" "gcc" "CMakeFiles/struktury_danych.dir/source/vector.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/struktury_danych.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/struktury_danych.dir/source/graph_adjacent_list.cpp.obj"
+  "CMakeFiles/struktury_danych.dir/source/graph_adjacent_list.cpp.obj.d"
+  "CMakeFiles/struktury_danych.dir/source/graph_two_dim_array.cpp.obj"
+  "CMakeFiles/struktury_danych.dir/source/graph_two_dim_array.cpp.obj.d"
+  "CMakeFiles/struktury_danych.dir/source/graph_vector_adjacent.cpp.obj"
+  "CMakeFiles/struktury_danych.dir/source/graph_vector_adjacent.cpp.obj.d"
+  "CMakeFiles/struktury_danych.dir/source/main.cpp.obj"
+  "CMakeFiles/struktury_danych.dir/source/main.cpp.obj.d"
+  "CMakeFiles/struktury_danych.dir/source/pair.cpp.obj"
+  "CMakeFiles/struktury_danych.dir/source/pair.cpp.obj.d"
+  "CMakeFiles/struktury_danych.dir/source/vector.cpp.obj"
+  "CMakeFiles/struktury_danych.dir/source/vector.cpp.obj.d"
+  "libstruktury_danych.dll.a"
+  "struktury_danych.exe"
+  "struktury_danych.exe.manifest"
+  "struktury_danych.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/struktury_danych.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
